3c05bcd80fa0ef2ec126c5b43a2bfd90fcacb52b
Matheus Almeida <matheusalmeida016@gmail.com>
2021-01-05 03:08:03 -0400

    Add one panel at a time

--- flex-panel-gallery/index.js
+++ flex-panel-gallery/index.js
@@ -5,20 +5,28 @@ const app = (function() {
     },
     
     expand: function expand() {
-      const panels = document.querySelectorAll('.panel');
-      
-      panels.forEach(panel => panel.addEventListener('click', app.toggleOpen));
-      panels.forEach(panel => panel.addEventListener('transitionend', app.toggleOpenActive));
+      app.panels().forEach(panel => panel.addEventListener('click', app.toggleOpen));
+      app.panels().forEach(panel => panel.addEventListener('transitionend', app.toggleOpenActive));
     },
     
     toggleOpen: function toggleOpen() {
       this.classList.toggle('open');
+      
+      app.panels().filter(panel => panel !== this).forEach(app.closeRemaining);
     },
     
     toggleOpenActive: function toggleOpenActive(e) {
       if(e.propertyName.includes('flex')) {
         this.classList.toggle('open-active');
       }
+    },
+    
+    panels: function panels() {
+      return Array.from(document.querySelectorAll('.panel'));
+    },
+    
+    closeRemaining: function closeRemaining(panel) {
+      panel.classList.remove('open');
     }
   };
 })();
