ea6027de0e1c943049dcee1268f778215a1d1c94
Matheus Almeida <matheusalmeida016@gmail.com>
2021-03-08 02:27:19 -0400

    Populate voices list

--- speech-synthesis/scripts.js
+++ speech-synthesis/scripts.js
@@ -1,6 +1,23 @@
 const msg = new SpeechSynthesisUtterance();
-let voices = [];
+const syth = window.speechSynthesis;
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  const voices = syth.getVoices();
+  
+  voicesDropdown.innerHTML = voices
+    .map(voice => `<option name="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+populateVoices();
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoices;
+}
+
