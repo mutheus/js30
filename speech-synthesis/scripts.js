9b0c463c92f030fe889521a05b58f400a9a6e4d9
Matheus Almeida <matheusalmeida016@gmail.com>
2021-03-09 04:14:53 -0400

    speechSynthesis on top of the window object

--- speech-synthesis/scripts.js
+++ speech-synthesis/scripts.js
@@ -1,5 +1,5 @@
 const msg = new SpeechSynthesisUtterance();
-const syth = window.speechSynthesis;
+const synth = window.speechSynthesis;
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
@@ -8,16 +8,38 @@ const stopButton = document.querySelector('#stop');
 msg.text = document.querySelector('[name="text"]').value;
 
 function populateVoices() {
-  const voices = syth.getVoices();
+  voices = synth.getVoices();
   
   voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes('en'))
     .map(voice => `<option name="${voice.name}">${voice.name} (${voice.lang})</option>`)
     .join('');
 }
 
 populateVoices();
-
 if (speechSynthesis.onvoiceschanged !== undefined) {
   speechSynthesis.onvoiceschanged = populateVoices;
 }
 
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+  toggle();
+}
+
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
