24c3d5325b5914fafc07051c483e677172625e72
Matheus Almeida <matheusalmeida016@gmail.com>
2021-03-11 06:02:17 -0400

    Challenge it self

--- sticky-nav/scripts.js
+++ sticky-nav/scripts.js
@@ -1,1 +1,15 @@
-console.log('hi')
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+window.addEventListener('scroll', fixNav);
