509dbf6efe5663272bb38bebd194810e9b2e71d2
Matheus Almeida <matheusalmeida016@gmail.com>
2021-03-17 06:02:48 -0400

    Starting challenge

--- stripe-follow-along-nav/scripts.js
+++ stripe-follow-along-nav/scripts.js
@@ -1,1 +1,18 @@
-console.log('hi')
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+function handleEnter() {
+  this.classList.add('trigger-enter');
+  
+  setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+  
+  background.classList.add('open');
+}
+
+function handleLeave() {
+  
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
